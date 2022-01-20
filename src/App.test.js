4ba4a40e22dc2e7079f174f2ba2ea27c0310ffef
Felipe Barsaga Avila <philipatdev@gmail.com>
2022-01-20 19:18:35 +0200

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import SocialNetworkApp from "./App";
-
-it('App Component is rendering fine', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SocialNetworkApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
-})
